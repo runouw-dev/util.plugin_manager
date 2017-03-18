c6bd508e266ee00d82cdf726643264e523d5f2e4
rhewitt <runouw@gmail.com>
2017-03-17 17:45:20 -0700

    - Updated test cases

--- src/test/java/newPlugin/TestNewPlugins.java
+++ src/test/java/newPlugin/TestNewPlugins.java
@@ -7,12 +7,10 @@ package newPlugin;
 
 import com.longlinkislong.plugin.BasicPluginHandler;
 import com.longlinkislong.plugin.Plugin;
-import com.longlinkislong.plugin.PluginHandler;
 import com.longlinkislong.plugin.PluginScanner;
-import java.util.Optional;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -38,6 +36,27 @@ public class TestNewPlugins {
     }
     
     @Plugin
+    public static class CatModded implements Animal{
+
+        private static boolean MOD_LOADED = false;
+        
+        @Plugin.Lookup
+        public static final String PLUGIN_ID = "Cat";
+        
+        @Plugin.OnLoad
+        public static void init(){
+            MOD_LOADED = true;
+        }
+        
+        public CatModded() {
+        }
+        
+        
+        @Override
+        public String say() { return "Bzzrrt"; }
+    }
+    
+    @Plugin
     public static class Dog implements Animal{
         
         @Plugin.Lookup
@@ -52,18 +71,74 @@ public class TestNewPlugins {
     }
     
     @Test
-    public void Test1(){
+    public void TestBasic(){
         PluginScanner scanner = new PluginScanner();
         scanner.addMetaPlugin(new BasicPluginHandler(Animal.class));
         
         scanner.scan(Cat.class, Dog.class);
-        
-        PluginHandler handler = scanner.getPluginHandler(Animal.class).get();
         
         Animal cat = scanner.newInstance(Animal.class, "Cat").get();
         Animal dog = scanner.newInstance(Animal.class, "Dog").get();
         
         assertEquals(cat.say(), "Meow");
         assertEquals(dog.say(), "Woof");
+    }
+    
+    @Test
+    public void TestUpdate(){
+        PluginScanner scanner = new PluginScanner();
+        scanner.addMetaPlugin(new BasicPluginHandler(Animal.class));
+        
+        scanner.scan(Cat.class, Dog.class);
+        
+        Animal cat = scanner.newInstance(Animal.class, "Cat").get();
+        Animal dog = scanner.newInstance(Animal.class, "Dog").get();
+        
+        assertEquals(cat.say(), "Meow");
+        assertEquals(dog.say(), "Woof");
+        
+        // simulating the old plugin being updated by a new one
+        // such as a run-time update and refresh
+        scanner.scan(CatModded.class);
+        
+        cat = scanner.newInstance(Animal.class, "Cat").get();
+        assertEquals(cat.say(), "Bzzrrt");
+        
+        assertTrue(CatModded.MOD_LOADED);
+    }
+    
+    @Test
+    public void TestOnLoad(){
+        PluginScanner scanner = new PluginScanner();
+        scanner.addMetaPlugin(new BasicPluginHandler(Animal.class));
+        
+        scanner.scan(CatModded.class);
+        
+        Animal cat = scanner.newInstance(Animal.class, "Cat").get();
+        
+        assertTrue(CatModded.MOD_LOADED);
+    }
+    
+    @Test
+    public void TestAll(){
+        PluginScanner scanner = new PluginScanner();
+        scanner.addMetaPlugin(new BasicPluginHandler(Animal.class));
+        
+        scanner.scan(Cat.class, Dog.class);
+        
+        Animal cat = scanner.newInstance(Animal.class, "Cat").get();
+        Animal dog = scanner.newInstance(Animal.class, "Dog").get();
+        
+        assertEquals(cat.say(), "Meow");
+        assertEquals(dog.say(), "Woof");
+        
+        // simulating the old plugin being updated by a new one
+        // such as a run-time update and refresh
+        scanner.scan(CatModded.class);
+        
+        cat = scanner.newInstance(Animal.class, "Cat").get();
+        assertEquals(cat.say(), "Bzzrrt");
+        
+        assertTrue(CatModded.MOD_LOADED);
     }
 }
