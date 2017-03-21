f8109284f89af85c627f1e770f60b859f51f1c40
zmichaels <zmichaels11@gmail.com>
2017-03-21 15:59:21 -0700

    - added better debugging to SPITest

--- src/test/java/spitest/SPITest.java
+++ src/test/java/spitest/SPITest.java
@@ -5,10 +5,14 @@
  */
 package spitest;
 
+import com.longlinkislong.plugin.PluginDescriptor;
 import com.longlinkislong.plugin.PluginScanner;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Example of SPI implementation. The service implementation is specified in
@@ -16,8 +20,7 @@ import org.junit.Test;
  *
  * @author zmichaels
  */
-public class SPITest {
-
+public class SPITest {    
     static {
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug");
     }
@@ -25,8 +28,10 @@ public class SPITest {
     @Test
     public void testBasic() {
         final PluginScanner scanner = new PluginScanner();
-
-        scanner.scan(Cat.class, Dog.class);
+        final Logger logger = LoggerFactory.getLogger(SPITest.class);
+        final List<PluginDescriptor> loadedPlugins = scanner.scan(Cat.class, Dog.class);
+        
+        logger.info("Loaded plugins: {}", loadedPlugins);        
 
         assertEquals("meow", scanner.newInstance(Animal.class, "plugins:cat").get().say());
         assertEquals("woof", scanner.newInstance(Animal.class, "plugins:dog").get().say());
@@ -35,13 +40,17 @@ public class SPITest {
     @Test
     public void testUpdate() {
         final PluginScanner scanner = new PluginScanner();
-
-        scanner.scan(Cat.class, Dog.class);
+        final Logger logger = LoggerFactory.getLogger(SPITest.class);
+        final List<PluginDescriptor> loadedPlugins = scanner.scan(Cat.class, Dog.class);
+        
+        logger.info("Loaded plugins: {}", loadedPlugins);
 
         assertEquals("meow", scanner.newInstance(Animal.class, "plugins:cat").get().say());
         assertEquals("woof", scanner.newInstance(Animal.class, "plugins:dog").get().say());
 
-        scanner.scan(DogModded.class);
+        final List<PluginDescriptor> updatedPlugins = scanner.scan(DogModded.class);
+        
+        logger.info("Updated plugin: {}", updatedPlugins);
 
         assertFalse("woof".equals(scanner.newInstance(Animal.class, "plugins:dog").get().say()));
     }
